30f9fcb708e6a3dec43807416d0def4088c88899
yongga <yonggagoh@gmail.com>
2018-10-13 22:47:13 +0800

    Changes made: - Added PHP Scripts for connecting to db
    conntester.php: main file to test connection

---
+++ app/PHP Scripts/conntester.php
@@ -0,0 +1,7 @@
+<?php
+//require_once 'D:\wamp64\www\iPlanner\include\DB_Connect.php';
+require_once(__DIR__.'/../iPlanner\include\DB_Connect.php');
+$db = new DB_Connect();
+//$db->connect();
+
+?>
---
+++ app/PHP Scripts/include/DB_Connect.php
@@ -0,0 +1,50 @@
+<?php
+ 
+/**
+ * A class file to connect to database
+ */
+
+class DB_CONNECT {
+	
+    // constructor
+    function __construct() {
+        // connecting to database
+        $this->connect();
+    }
+ 
+    // destructor
+    function __destruct() {
+        // closing db connection
+        //$this->close();
+    }
+ 
+    /**
+     * Function to connect with database
+     */
+    function connect() {
+		
+        // import database connection variables
+        require_once('D:\wamp64\www\iPlanner\include\DB_Config.php');
+ 
+        // Connecting to mysql database
+        $con = mysqli_connect(DB_SERVER, DB_USER, DB_PASSWORD) or die(mysqli_error());
+ 
+        // Selecing database
+        $db = mysqli_select_db($con, DB_DATABASE) or die(mysqli_error($con)) or die(mysqli_error($con));
+        
+		// returing connection cursor
+		echo "Connected liao la cb!";
+        return true;
+    }
+ 
+    /**
+     * Function to close db connection
+     */
+    /* function close() {
+        // closing db connection
+        mysqli_close($con);
+    } */
+ 
+}
+ 
+?>
