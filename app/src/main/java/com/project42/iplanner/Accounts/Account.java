339ef2a873a3700dfb902b9ef56a890107fc8651
Milkweedz <Milkweedz@hotmail.com>
2018-10-26 12:28:27 +0800

    Meddling around with backend stuff Tried to make a file for SQL_Post. Backend doesn't work yet, but the app runs and the front end is fine.

--- app/src/main/java/com/project42/iplanner/Accounts/Account.java
+++ app/src/main/java/com/project42/iplanner/Accounts/Account.java
@@ -1,10 +1,13 @@
 package com.project42.iplanner.Accounts;
 
+import com.project42.iplanner.DBConn.DBController;
+
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 import java.sql.Statement;
 import java.sql.ResultSet;
+
 
 public class Account {
 
